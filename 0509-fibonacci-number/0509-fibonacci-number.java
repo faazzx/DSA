3a9c43d623e6c62d6c6eee547c463f6df5271323
Mohd faaz khan <mdfaazkhan786@gmail.com>
2025-05-09 20:53:16 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,26 @@
-class Solution 
-{
-    public int fib(int N)
-    {
-        if(N <= 1)
-            return N;
+class Solution {
+    public int fib(int n) {
+        int [] dp= new int [n+1];
+         Arrays.fill(dp, -1);
+         solver(dp , n);
+         return dp[n];
 
-		int[] fib_cache = new int[N + 1];
-		fib_cache[1] = 1;
-
-		for(int i = 2; i <= N; i++)
-		{
-			fib_cache[i] = fib_cache[i - 1] + fib_cache[i - 2];
-		}
-		return fib_cache[N];
+    }
+    int solver(int[] dp , int n){
+        if( dp[n]!=-1){
+            return dp[n];
+        
+    }
+    else {
+        if(n==0 || n==1){
+            dp[n]=n;
+        }
+        else {
+            
+              dp[n]=solver(dp, n-1)+solver(dp, n-2);
+            
+        }
+         return dp[n];
+    }
     }
 }
