4c380cd48c9d3fb7e2e152be7293a4d310c1d37f
Mohd faaz khan <mdfaazkhan786@gmail.com>
2025-08-27 15:41:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,18 @@
 class Solution {
-     int a=0;
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-         
-         return solver( dp , 0 , 0, m , n);
+        int [][] dp = new int[m][n];
+        return solver(dp , 0, 0, m,n);
     }
 
-
-
-    public int solver(int[][] dp , int i , int j , int m , int n){
-        if( i==m-1 && j==n-1) {
-            return 1;
-        }
-        if(i==m ||  j== n) return 0;
-
-        if(dp[i][j]==0){
-
-            int down = solver(dp, i+1, j,m,n);
-            int left= solver(dp, i,j+1, m,n);
-            dp[i][j]= down+left;
-        return dp[i][j];
-        }
-        else{
-            return dp[i][j];
-        }
+    int solver(int[][] dp , int i , int j,int m , int n){
+        if(i==m-1 && j==n-1) return 1;
         
-         
-        
-        
-       
+        if(i>=m || j>=n ) return 0;
+        if(dp[i][j]!=0 ) return dp[i][j];
+
+
+        int down = solver(dp , i, j+1, m,n);
+        int right = solver(dp , i+1, j, m,n);
+         return down+right;
     }
 }
