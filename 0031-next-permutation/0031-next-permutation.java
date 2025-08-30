63514824851ca13560be42fea5cfa358a378de51
Mohd faaz khan <mdfaazkhan786@gmail.com>
2025-08-30 15:34:25 +0530

    Time: 0 ms (100%), Space: 43 MB (89.17%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,45 +1,57 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        
+        // finding breakpoing ie first element that is bigger than its previous one 
+        int size= nums.length-1;
         int bp=-1;
-        //finding breaking point
-        for(int i=nums.length-2;i>=0;i--){
+        for(int i=size-1 ;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 bp=i;
                 break;
             }
         }
+
+        // if there is no break point then reverse the whole array and return 
         if(bp==-1){
-            int a=0;
-            int b=nums.length-1;
-            while(a<b){
-                int temp=nums[a];
-                nums[a]=nums[b];
-                nums[b]=temp;
-                a++;
-                b--;
-            }
-            return;
+        int a=0;
+        int b=size;
+        while(a<b){
+            int temp = nums[a];
+            nums[a]= nums[b];
+            nums[b]=temp;
+            a++;
+            b--;
         }
-        //finding index in the array
-        int smallest=nums[bp];
-        for(int i=nums.length-1;i>=0;i--){
+        return;
+        }
+
+
+        // if we find the breaking point then 
+        // find the first index from the right which is greater than the number at breaking point then swap it with breaking point 
+
+        int smallest = bp;
+        for(int i=size ; i>=0;i--){
             if(nums[i]>nums[bp]){
-                smallest=i;
-                break;
+                smallest = i;
+                 break;
             }
         }
-        //swapping
-        int temp=nums[smallest];
-        nums[smallest]= nums[bp];
-        nums[bp]=temp;
-        int x=bp+1;
-        int y=nums.length-1;
-        while(x<y){
-            int tep=nums[x];
-            nums[x]=nums[y];
-            nums[y]=tep;
+
+        // now swapping starts 
+         int temp = nums[smallest];
+         nums[smallest]= nums[bp];
+         nums[bp]= temp;
+         int x=bp+1;
+         int y=size;
+         while(x<y){
+            int tem = nums[x];
+            nums[x]= nums[y];
+            nums[y]= tem; 
             x++;
             y--;
-        }
+         }
+          return ;
+
+
     }
 }
