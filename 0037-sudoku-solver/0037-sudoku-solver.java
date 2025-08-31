d0ba3a38ada031ae263df50a1fba7af48dc3b4a5
Mohd faaz khan <mdfaazkhan786@gmail.com>
2025-08-31 11:16:55 +0530

    Time: 205 ms (63.34%), Space: 41.3 MB (33.65%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solver(board);
+    }
+
+    boolean solver(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char k = '1'; k <= '9'; k++) {
+
+                        if (isvalid(board, i, j, k)) {
+                            board[i][j] = k;
+                            if (solver(board))
+                                return true;
+
+                            board[i][j] = '.';
+
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean isvalid(char[][] board, int i, int j, char num) {
+        for (int c = 0; c < 9; c++) {
+            if (board[i][c] == num || board[c][j] == num)
+                return false;
+
+            
+        }
+        int startRow = (i / 3) * 3;
+            int startCol = (j / 3) * 3;
+            for (int k = startRow; k < startRow + 3; k++) {
+                for (int l = startCol; l < startCol + 3; l++) {
+                    if (board[k][l] == num)
+                        return false;
+                }
+            }
+        return true;
+    }
+}
